edc3ad6db0fda1fd772e6a092cae3d0c7cc27b71
sounishnath003 <flock.sinasini@gmail.com>
2019-03-21 18:55:04 +0530

    todos index and props

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="max-w-sm shadow-lg p-6 m-auto justify-center items-center">
+    <div class="flex flex-row flex-wrap p-4 bg-white mb-6 border-b justify-between items-center">
+      <div class="icon">
+        <i class="fas text-indigo fa-angle-left"></i>
+      </div>
+      <h2 class="text-lg inline-flex tracking-loose text-indigo-dark">MONDAY, <p class="text-indigo opacity-75 ml-3">14th march</p></h2>
+      <div class="icon">
+        <i class="fas text-indigo fa-angle-right"></i>
+      </div>
+    </div>
+
+    <Todos v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos_index.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Morning Run',
+          time: '7:30 am',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Meeting',
+          time: '12:30 pm',
+          completed: true
+        },
+        {
+          id: 3,
+          title: 'Launch with Mike',
+          time: '02:30 pm',
+          completed: false
+        }
+      ]
+    }
+  },
 }
 </script>
 
 <style>
+@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.0/css/all.css');
+@import './mainly.css';
+
+*{
+  font-family: product sans;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Todos_index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2 class="text-indigo-dark tracking-loose">Todays</h2>
+    <div class="flex flex-col">
+      <div class="div inline-flex flex-wrap" v-bind:key="todo.id" v-for="todo in todos">
+        <h3> {{todo.title}} </h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Todos',
+  props: ['todos'],
+}
+</script>
